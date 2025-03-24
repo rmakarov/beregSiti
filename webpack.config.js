05789f61524c2de332ea4869ee82a9a280ae7d88
Ruslan Makarov <rmakarov@sprinterra.ru>
2025-03-24 17:25:17 +0300

    Added html-bundler-webpack-plugin/ Added some styles

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,20 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlBundlerPlugin = require('html-bundler-webpack-plugin');
+const mode = process.env.NODE_ENV || 'development'
 
 module.exports = {
-    mode: 'development',
+    mode: mode,
     context: path.resolve(__dirname, 'app'),
-    entry: './js/index.js',
+   /* entry: './js/main.js',*/
+    module: {
+        rules: [
+            {
+                test: /\.(sa|sc|c)ss$/i,
+                use: ["css-loader"],
+            },
+        ],
+    },
     devServer: {
         static: {
             directory: path.join(__dirname, 'public'),
@@ -12,8 +22,21 @@ module.exports = {
         compress: true,
         port: 9000,
     },
-    plugins: [new HtmlWebpackPlugin({
-        template: './static/index.html',
-        filename: './index.html'
-    })],
+    plugins: [
+        new HtmlBundlerPlugin({
+            // specify the entry points for HTML pages (or a template)
+            entry: {
+                index: './index.html', // save generated HTML into dist/index.html
+            },
+            js: {
+                filename: 'js/main.js', // JS output filename
+            },
+            css: {
+                filename: 'css/main.css', // CSS output filename
+            },
+        }),
+       /* new HtmlWebpackPlugin({
+        template: './index.html',
+        filename: './index.html'})*/
+    ],
 };
